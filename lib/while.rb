14ab883ccee821e1d4466c93480fb4ab18078022
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-24 22:45:47 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
 def using_while
-	levitation_force = 6
-	
-	#your code here
+  levitation_force = 6
+  while levitation_force < 10
+    puts "Wingardium Leviosa!"
+    levitation_force += 1
+    puts "Your levitation_force is now #{levitation_force}."
+    end
+  puts "Our feather is flyin' high! Hermione is so so proud :)"
 end
 
-
+using_while
